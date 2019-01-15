c3fd48515c5821e0af62ea92f5b807e200063d88
Seungbin Paek <noely1008@naver.com>
2019-01-15 19:30:28 +0900

    boards

--- HUSproject/urls.py
+++ HUSproject/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+app_name = 'boards'
+urlpatterns = [ #mapper: 해당하는 view 함수에 대한 mapping 목록을 정의한다.
+       path('admin/', admin.site.urls),
+       path('boards/', include('boards.urls')),
+    ]
