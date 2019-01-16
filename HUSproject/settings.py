50de26e0ed6b03115c10db15666cdd8e4577af60
yejineee <21700431@handong.edu>
2019-01-16 15:30:00 +0900

    settings.py, urls.py 추가
    Signed-off-by: yejineee <21700431@handong.edu>

--- HUSproject/settings.py
+++ HUSproject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mypage.apps.MypageConfig',
+    'join.apps.JoinConfig',
 ]
 
 MIDDLEWARE = [
