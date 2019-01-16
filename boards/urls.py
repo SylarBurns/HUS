27e45e9701cd40f47c6f393add6c42691c6f698f
SeungYunLee <sokon954@naver.com>
2019-01-16 16:30:20 +0900

    linking all the pages

--- boards/urls.py
+++ boards/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from . import views
-
+app_name = 'boards'
 urlpatterns = [
     path('', views.ListView.as_view(), name='list'),
     path('detail/', views.DetailView.as_view(), name='detail'),
