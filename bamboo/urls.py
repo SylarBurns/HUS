043cf70cb68803ac16ad374c8ccc1e5f52dba50c
SeungYunLee <sokon954@naver.com>
2019-01-22 17:39:42 +0900

    좋아요 싫어요 기능 추가

--- bamboo/urls.py
+++ bamboo/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.bambooListView.as_view(), name='bambooList'),
     path('<int:pk>/', views.bambooDetailView.as_view(), name='bambooDetail'),
     path('create/', views.bambooCreateView.as_view(), name='bambooCreate'),
+    path('<int:pk>/like/',views.postLike, name="postLike"),
+    path('<int:pk>/disLike/',views.postDislike, name="postDislike"),
     # path('<int:pk>/rewrite/', views.bambooUpdateView.as_view(), name='bambooRewrite'),
 ]
--- bamboo/views.py
+++ bamboo/views.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import(
+    View,
     CreateView,
     DetailView,
     ListView,
@@ -11,7 +13,7 @@ from django.urls import reverse
 from heart.models import Post, PostRelation, User
 from .forms import PostModelForm
 from django.utils import timezone
-
+from django.core import exceptions
 class bambooListView(ListView):
     template_name = 'bamboo/boardList.html'
     queryset= Post.objects.all().filter(boardNum__exact=3).order_by('-pk')
@@ -43,3 +45,36 @@ class bambooUpdateView(UpdateView):
     def get_object(self):
         id_ = self.kwargs.get("pk")
         return get_object_or_404(Post, id=id_)
+
+def postLike(request, pk):#좋아요 싫어요 기능 추가
+    post = Post.objects.get(pk=pk)#pk로 post object를 가져온다.
+    try : 
+        relation = PostRelation.objects.filter(post_id=pk).filter(user_id=2).get()
+        if relation.like :
+            relation.like = False
+        else:
+            relation.like = True
+            relation.dislike = False
+        relation.save()
+    except exceptions.ObjectDoesNotExist :
+        relation = PostRelation(post=post, user=User.objects.get(pk=2), dislike=True)
+        relation.save()
+        
+    return redirect('bamboo:bambooDetail', pk=pk)
+
+def postDislike(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    try : 
+        relation = PostRelation.objects.filter(post_id=pk).filter(user_id=2).get()
+        if relation.dislike:
+            relation.dislike=False
+        else:
+            relation.like=False
+            relation.dislike=True
+        relation.save()
+    except :
+        relation = PostRelation(post=post, user=User.objects.get(pk=2), dislike=True)
+        relation.save()
+        
+    return redirect('bamboo:bambooDetail', pk=pk)
